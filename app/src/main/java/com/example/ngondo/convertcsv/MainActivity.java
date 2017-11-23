6f9761c3582843fc222729fada2ede2416494b1b
cngondo <devngondo@gmail.com>
2017-11-23 04:18:36 +0300

    Added the background task to perform with AsyncTask

--- app/src/main/java/com/example/ngondo/convertcsv/MainActivity.java
+++ app/src/main/java/com/example/ngondo/convertcsv/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ngondo.convertcsv;
 
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
@@ -9,18 +10,27 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final int ACTIVITY_CHOOSE_FILE = 1; //set the intent choose to always open the file chooser dialog
+    public static final int ACTIVITY_CHOOSE_FILE = 1; //set the intent choose to always open the file chooser
 
     //DB Varianblles
-    DBController dbController;
-    SQLiteDatabase db;
+    public DBController dbController;
+    public SQLiteDatabase db;
+
+    public String filePath; //path to the csv file
 
 
     @Override
@@ -54,18 +64,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     /*
-    * Action after the selection of the csv file, we need to inmpoirt the data to our database
+    *
     * */
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    /*
+        * Action after the selection of the csv file, we need to inmpoirt the data to our database
+        * */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        super.onActivityResult(requestCode, resultCode, data);
 
         switch(requestCode) {
             case ACTIVITY_CHOOSE_FILE: {
+                filePath = data.getData().getPath();
+                db.beginTransaction();
+
                 if (resultCode == RESULT_OK) {
                     //AsyncTask for importing the values to the sqlite db
-                   // Toast.makeText(getApplicationContext(),"Intent chooser works well", Toast.LENGTH_SHORT).show();
+                    try {
+                        new ImportCSV().execute("");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+//                    Toast.makeText(getApplicationContext(),"Intent chooser works well", Toast.LENGTH_SHORT).show();
+                    Log.d("FILE_UPLOAD",filePath);
 
                 }
             }
@@ -100,6 +128,16 @@ public class MainActivity extends AppCompatActivity {
     class ImportCSV extends AsyncTask<String, String, String> {
 
         ProgressDialog progressDialog;
+        File csvFile = new File(filePath);
+        BufferedReader fileReader = new BufferedReader(new FileReader(csvFile));
+        String line = ""; //initialize empty string to read the values
+
+        ContentValues cv = new ContentValues();
+
+        //Throws this exception if the file is not found, or the wrong format is chosen
+        ImportCSV() throws FileNotFoundException {
+            Toast.makeText(getApplicationContext(), "CSV File not found. Failed to import. Try Again", Toast.LENGTH_SHORT).show();
+        }
 
         @Override
         protected void onPreExecute() {
@@ -110,8 +148,33 @@ public class MainActivity extends AppCompatActivity {
         // Import the csv file using content values
         @Override
         protected String doInBackground(String... strings) {
+            try {
+                while((line = fileReader.readLine())!= null){
+                    String[] str = line.split(",", 3); //Split the values to the three columns
 
+                    String admno = str[0].toString();
+                    String name = str[1].toString();
+                    String pnumber = str[2].toString();
 
+                    /*
+                    * Input the values into content Values
+                    * */
+                    cv.put(DBModel.Columns.ADMNO, admno);
+                    cv.put(DBModel.Columns.STUDENTNAME, name);
+                    cv.put(DBModel.Columns.PARENTNO, pnumber);
+
+                    db.insert(DBModel.DBNAME, null, cv);
+                }
+                db.setTransactionSuccessful();
+                db.endTransaction();
+
+            } catch (IOException e) {
+                if(db.inTransaction()){
+                    db.endTransaction();
+                    progressDialog.setMessage(e.getMessage().toString() + "first");
+                    progressDialog.show();
+                }
+            }
             return null;
         }
 
